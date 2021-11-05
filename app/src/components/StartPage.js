7f68a6f779d913d7852e04682c06699093bf3895
Emma Dawson <emma.l.dawson@gmail.com>
2021-11-05 10:22:18 +0100

    Add randomize flags under feature flag

--- app/src/components/StartPage.js
+++ app/src/components/StartPage.js
@@ -5,108 +5,130 @@ import { customAlphabet } from 'nanoid'
 import * as utils from '../utils'
 
 import { db } from '../App'
+import countries from '../countries'
 
 import { ref, update } from 'firebase/database'
 import { useObject } from 'react-firebase-hooks/database'
 
 const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvxyz', 5)
 
+const countriesArr = Object.keys(countries)
 
+const featureFlags = JSON.parse(localStorage.getItem('featureFlags'))
 
 const StartPage = () => {
-    const [snapshot, loading, error] = useObject(ref(db, 'nextGame'))
-    const [location, setLocation] = useLocation()
-  
-    if (loading) return <div className="fw6 fs5">Loading...</div>
-    const nextGame = snapshot.val()
-  
-    const play = async () => {
-      if (R.isNil(nextGame)) {
-        const updates = {}
-        const gameId = nanoid()
-        updates['/nextGame'] = gameId
-        await update(ref(db), updates)
-        setLocation(`/game/${gameId}/1`)
-      } else {
-        const game = utils.createGame()
-        const updates = {}
-        updates['/nextGame'] = null
-        updates[`/games/${nextGame}`] = game
-        await update(ref(db), updates)
-        setLocation(`/game/${nextGame}/2`)
-  
-        await utils.sleep(3000)
-        const updates2 = {}
-        updates2[`/games/${nextGame}/status`] = 'playing'
-        await update(ref(db), updates2)
-      }
+  const [snapshot, loading, error] = useObject(ref(db, 'nextGame'))
+  const [location, setLocation] = useLocation()
+
+  const randomNum = Math.floor(Math.random() * 156 + 1)
+  console.log(randomNum)
+  const countriesArrSlice = countriesArr.slice(randomNum, randomNum + 60)
+  console.log(countriesArrSlice)
+  if (loading) return <div className="fw6 fs5">Loading...</div>
+  const nextGame = snapshot.val()
+
+  const play = async () => {
+    if (R.isNil(nextGame)) {
+      const updates = {}
+      const gameId = nanoid()
+      updates['/nextGame'] = gameId
+      await update(ref(db), updates)
+      setLocation(`/game/${gameId}/1`)
+    } else {
+      const game = utils.createGame()
+      const updates = {}
+      updates['/nextGame'] = null
+      updates[`/games/${nextGame}`] = game
+      await update(ref(db), updates)
+      setLocation(`/game/${nextGame}/2`)
+
+      await utils.sleep(3000)
+      const updates2 = {}
+      updates2[`/games/${nextGame}/status`] = 'playing'
+      await update(ref(db), updates2)
     }
-    return (
-      <div className="page">
-        <div className="st-flags">
-          <div className="f32">
-            <div className={`flag aze`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bih`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag brb`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag swe`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bgd`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bel`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bfa`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bgr`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bhr`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bdi`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag ben`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bmu`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag brn`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bol`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bra`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bhs`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag btn`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag fra`}></div>
-          </div>
-          <div className="f32">
-            <div className={`flag bwa`}></div>
-          </div>
+  }
+
+  const flags =
+    featureFlags.randomizeFlags === false ? (
+      <div className="st-flags">
+        <div className="f32">
+          <div className={`flag aze`}></div>
         </div>
-        <div className="button btn-square" onClick={play}>
-          START
+        <div className="f32">
+          <div className={`flag bih`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag brb`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag swe`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bgd`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bel`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bfa`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bgr`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bhr`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bdi`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag ben`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bmu`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag brn`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bol`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bra`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bhs`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag btn`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag fra`}></div>
+        </div>
+        <div className="f32">
+          <div className={`flag bwa`}></div>
         </div>
       </div>
+    ) : (
+      <div className="st-flags">
+        {countriesArrSlice.map(country => (
+          <div className="f32" key={country}>
+            <div className={`flag ${country.toLowerCase()}`}></div>
+          </div>
+        ))}
+      </div>
     )
-  }
+
+  return (
+    <div className="page">
+      {flags}
+
+      <div className="button btn-square" onClick={play}>
+        START
+      </div>
+    </div>
+  )
+}
 
   export default StartPage
