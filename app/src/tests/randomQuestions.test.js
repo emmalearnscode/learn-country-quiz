74e6deec35efc09dd17ea1a6ac44db790b0f2f55
Emma Dawson <emma.l.dawson@gmail.com>
2021-11-10 15:09:40 +0100

    Updated tests to work with different numbers of questions

--- app/src/tests/randomQuestions.test.js
+++ app/src/tests/randomQuestions.test.js
@@ -3,8 +3,8 @@ import assert from 'assert'
 import { randomGeneratedQuestions } from '../utils.js'
 
 describe('randomGeneratedQuestions', () => {
-  it('should not ask about the same flag', () => {
-    const questions = randomGeneratedQuestions()
+  it('should return 5 different questions', () => {
+    const questions = randomGeneratedQuestions(5)
 
     let correctAnswers = []
     for (const key in questions) {
@@ -13,8 +13,28 @@ describe('randomGeneratedQuestions', () => {
     const setArr = new Set(correctAnswers)
     assert.equal(setArr.size, 5)
   })
+  it('should return 10 different questions', () => {
+    const questions = randomGeneratedQuestions(10)
+
+    let correctAnswers = []
+    for (const key in questions) {
+      correctAnswers.push(questions[key].correct)
+    }
+    const setArr = new Set(correctAnswers)
+    assert.equal(setArr.size, 10)
+  })
+  it('should return 15 different questions', () => {
+    const questions = randomGeneratedQuestions(15)
+
+    let correctAnswers = []
+    for (const key in questions) {
+      correctAnswers.push(questions[key].correct)
+    }
+    const setArr = new Set(correctAnswers)
+    assert.equal(setArr.size, 15)
+  })
   it('should not repeat alternatives', () => {
-    const questions = randomGeneratedQuestions()
+    const questions = randomGeneratedQuestions(5)
 
     for (const key in questions) {
       const alternatives = Object.values(questions[key].alternatives)
